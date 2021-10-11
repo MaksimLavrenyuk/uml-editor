c1cfe8a493a2fc986ffc5f44864d53dbecfb61a1
MaksimLavrenyuk <maksim.lavrenyuk@outlook.com>
2021-10-11 23:06:02 +0300

    UML-27 fix ComponensList styles

--- src/components/DiagramEditor/ComponentsList/Component.tsx
+++ src/components/DiagramEditor/ComponentsList/Component.tsx
@@ -1,14 +1,14 @@
 import { I18n } from '@lingui/core';
 import { withI18n } from '@lingui/react';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import { ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import { DragEvent, ReactNode, useCallback } from 'react';
+import { DragEvent, useCallback } from 'react';
 import { diagramDragAndDropEvent } from '../Diagram/widgets/DiagramWidget';
 import { COMPONENTS_NAMES } from '../../../locales/lang-constants';
 import ComponentType from '../../../models/ComponentType';
 
 type ComponentProps = {
     i18n: I18n
-    icon: ReactNode
     componentType: ComponentType
 };
 
@@ -19,7 +19,7 @@ type ComponentProps = {
  * @see DiagramWidget
  */
 function Component(props: ComponentProps) {
-    const { componentType, icon, i18n } = props;
+    const { componentType, i18n } = props;
 
     const dragStartHandler = useCallback((event: DragEvent) => {
         event.dataTransfer.setData(diagramDragAndDropEvent.ADD, String(componentType));
@@ -32,7 +32,7 @@ function Component(props: ComponentProps) {
             button
         >
             <ListItemIcon>
-                {icon}
+                <DragIndicatorIcon />
             </ListItemIcon>
             <ListItemText primary={i18n._(COMPONENTS_NAMES[componentType])} />
         </ListItem>
--- src/components/DiagramEditor/ComponentsList/index.tsx
+++ src/components/DiagramEditor/ComponentsList/index.tsx
@@ -1,6 +1,5 @@
 import { useMemo } from 'react';
 import { AppBar, List } from '@mui/material';
-import { Inbox } from '@mui/icons-material';
 import classes from './ComponentsList.module.scss';
 import Component from './Component';
 import ComponentType from '../../../models/ComponentType';
@@ -11,31 +10,21 @@ import ComponentType from '../../../models/ComponentType';
  */
 function ComponentsList() {
     const components = useMemo(() => [
-        {
-            componentType: ComponentType.CLASS,
-            icon: <Inbox />,
-        },
-        {
-            componentType: ComponentType.INTERFACE,
-            icon: <Inbox />,
-        },
+        ComponentType.CLASS,
+        ComponentType.INTERFACE,
     ], []);
 
     return (
-        <div className={classes.list}>
-            <span>example string</span>
-            <AppBar color="default">
-                <List component="nav" aria-label="main mailbox folders">
-                    {components.map((component) => (
-                        <Component
-                            key={component.componentType}
-                            componentType={component.componentType}
-                            icon={<Inbox />}
-                        />
-                    ))}
-                </List>
-            </AppBar>
-        </div>
+        <AppBar color="default" className={classes.container}>
+            <List component="nav" aria-label="main mailbox folders">
+                {components.map((component) => (
+                    <Component
+                        key={component}
+                        componentType={component}
+                    />
+                ))}
+            </List>
+        </AppBar>
     );
 }
 
